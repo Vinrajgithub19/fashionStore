e6c3a323e799efc8106a4836cb5d585e80bf4728
Vinay kumar G <90076335+Vinrajgithub19@users.noreply.github.com>
2024-07-29 19:06:00 +0530

    Add files via upload
    these are the initial pages

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: Add an event listener for form submission
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent!');
+});
